19a513422add10adf4c6484bf9470d8c855ddcbd
akoranteng <akoranteng@msn.com>
2026-07-21 04:18:20 -0400

    feat(ui-styling): update WelcomePage & DetailsPage + add Module 02 README

--- MauiStarter2026/App.xaml.cs
+++ MauiStarter2026/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace MauiStarter2026;
+﻿using MauiStarter2026.Pages;
+
+namespace MauiStarter2026;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-
-        MainPage = new AppShell();
+        //MainPage = new WelcomePage();
+        InitializeComponent();
+        MainPage = new NavigationPage(new WelcomePage());
+    
     }
 }
--- MauiStarter2026/Pages/WelcomePage.xaml.cs
+++ MauiStarter2026/Pages/WelcomePage.xaml.cs
@@ -7,4 +7,9 @@ public partial class WelcomePage : ContentPage
     {
         InitializeComponent();
     }
+private async void OnGetStartedClicked(object sender, EventArgs e)
+{
+    await Navigation.PushAsync(new DetailsPage());
+}
+
 }
